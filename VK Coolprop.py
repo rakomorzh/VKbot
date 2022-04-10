9c7634c35f9e1c94e7292d0f10d3730e05f94fca
happyvovanaumov@gmail.com <Djdfy654>
2022-04-10 20:29:39 +0300

    Bot

--- VK Coolprop.py
+++ VK Coolprop.py
@@ -1,4 +1,5 @@
 import vk_api, random
+from CoolProp.CoolProp import PropsSI
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 # из лонг пулла импортируем нужные нам библиотеки.
@@ -19,8 +20,9 @@ while True:  # бесконечный цикл
             peer_id = event.obj['peer_id']
 
             if mess == "Привет!":  # если текст сообщения = Привет!, отправляем сообщение.
+                h_mess=PropsSI("H", "P", 101325, "T", 300, "HEOS::WATER")/1000
                 vk.method("messages.send",
-                          {"peer_id": peer_id, "message": "Прииивееетт!!", "random_id": random.randint(1, 2147483647)})
+                          {"peer_id": peer_id, "message": f"Энтальпия = {h_mess} кДж/кг" , "random_id": random.randint(1, 2147483647)})
                 # в строке выше из вк апи мы получаем метод отправки сообщения, а после указываем все нужные данные, такие как айди чата (лс/беседа), содержание отправленного ботом сообщения и рандомное айди для сообщения (а вообще я хз что это, никогда не обращал внимания).
 
                 # Вообщем-то все. Бот готов
